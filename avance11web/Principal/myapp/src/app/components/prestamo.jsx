6d0dc94ea6ee2c8fb0eaa72dc995b4f69d344a58
Mitchel <mitchel.mrtp@gmail.com>
2023-10-14 03:28:44 -0500

    ingreso final mrtp

--- avance11web/Principal/myapp/src/app/components/prestamo.jsx
+++ avance11web/Principal/myapp/src/app/components/prestamo.jsx
@@ -1,9 +1,19 @@
-import styles from './prestamo.module.css'
+"use client"
 import Sidebar from './sidebar';
+import Botonlibros from './busquedalibro';
 import Barra from './barra';
+import Libro from './libro';
 import Barrareserva from './barrareserva';
-import Barrarecurso from './barrarecurso'
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import Admins from "./Admin.json";
+import Alumnos from "./Alumnos.json"
+import styles from './prestamo.module.css'
+import Barrarecurso from './barrarecurso'
+import BusquedaAlumn from './AlumnBusqueda';
+import BusquedaAlumnRes from './AlumnBusquedaRes';
+import ResultadosAlumn from './AlumnResultados';
+import PaginaResultados from './PaginaResultados';
 
 
 const Prestamo = () =>{
@@ -32,23 +42,63 @@ const Prestamo = () =>{
     
       // Resto de tu código de la página de inicio del alumno
 
+      const [searchText, setSearchText] = useState('');
+      const [categories, setCategories] = useState([]);
+      const [libraryData, setLibraryData] = useState([]);
+      const [searchResults, setSearchResults] = useState([]);
+      const [showResults, setShowResults] = useState(false); // Nuevo estado para mostrar resultados
+    
+      useEffect(() => {
+        fetch('library.json')
+          .then((response) => response.json())
+          .then((data) => {
+            setLibraryData(data);
+          })
+          .catch((error) => {
+            console.error('Error al cargar library.json', error);
+          });
+      }, []);
+    
+      const handleSearch = (text, categories) => {
+        setSearchText(text);
+        setCategories(categories);
+    
+        const textResults = libraryData.filter((libro) =>
+          libro.titulo.toLowerCase().includes(text.toLowerCase())
+        );
+    
+        if (categories.length > 0) {
+          const categoryResults = libraryData.filter((libro) => {
+            const libroCategorias = (libro.categoria || '').toLowerCase();
+            return categories.some((category) => libroCategorias.includes(category.toLowerCase()));
+          });
+          setSearchResults(categoryResults);
+        } else {
+          setSearchResults(textResults);
+        }
+    
+        setShowResults(true); // Mostrar resultados después de la búsqueda
+      };
 
     return (
         <div className={styles.container}>
         <Barra titulobarra = 'Administración de Biblioteca'/>
         <main className={styles.xd}>
-            <Sidebar li1="Inicio" li2="Perfil" li3="Bibliotecas"/>
-            <section className={styles.parte2}>
-                <div className={styles.titulo2}>
-                    <span>Bienvenido, {nombre}!</span>
-                </div>
-            <article className={styles.articulo1}>
+        <Sidebar li1="Inicio" li2="Perfil" li3="Bibliotecas" />
+          {showResults ? (
+            <PaginaResultados results={searchResults} />
+          ) : (
+        <section className={styles.parte2}>
+          
+            <div className={styles.titulo2}>
+              <span>Búsqueda</span></div>
+              <article className={styles.articulo1}>
+                  
                 <div className={styles.recuadro1}>
-                    <div className={styles.Barrasprestamo}>
-                    <Barrareserva/>
-                    <Barrarecurso/>
-                    </div>
-                    <div className={styles.checkboxs}>
+                  <div className={styles.Barrasprestamo}>
+                    <BusquedaAlumn onSearch={handleSearch} />
+                  </div>
+                  <div className={styles.checkboxs}>
                         <span>Incluir busqueda</span>
                         <div>
                             <input type="checkbox" />
@@ -69,13 +119,14 @@ const Prestamo = () =>{
                     </div>
                     
                 </div>
-                <div className={styles.botones}>
-                        <button className={styles.boton1}>Limpiar</button>
-                        <button className={styles.boton2} >Buscar</button>
-                </div>
-            </article>
-            </section>
-        </main>
+        
+                
+              </article>
+            
+          
+        </section>
+        )}
+      </main>
     </div>
     )
 }
